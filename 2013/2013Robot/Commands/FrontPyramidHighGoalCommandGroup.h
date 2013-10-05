34d0fd2d73e82f740476a32e7fc389430eca7f74
Ryan Sjostrand <theyoutubesevergeek@gmail.com>
2013-10-05 16:35:31 -0500

    Added Some Stub Auto modes to fill in latter.

---
+++ 2013/2013Robot/Commands/FrontPyramidHighGoalCommandGroup.h
@@ -0,0 +1,16 @@
+#ifndef FRONTPYRAMIDHIGHGOALCOMMANDGROUP_H
+#define FRONTPYRAMIDHIGHGOALCOMMANDGROUP_H
+
+#include "Commands/CommandGroup.h"
+
+/**
+ *
+ *
+ * @author nowireless
+ */
+class FrontPyramidHighGoalCommandGroup: public CommandGroup {
+public:	
+	FrontPyramidHighGoalCommandGroup();
+};
+
+#endif
---
+++ 2013/2013Robot/Commands/FrontPyramidMiddleGoalCommandGroup.h
@@ -0,0 +1,16 @@
+#ifndef FRONTPYRAMIDMIDDLEGOALCOMMANDGROUP_H
+#define FRONTPYRAMIDMIDDLEGOALCOMMANDGROUP_H
+
+#include "Commands/CommandGroup.h"
+
+/**
+ *
+ *
+ * @author nowireless
+ */
+class FrontPyramidMiddleGoalCommandGroup: public CommandGroup {
+public:	
+	FrontPyramidMiddleGoalCommandGroup();
+};
+
+#endif
